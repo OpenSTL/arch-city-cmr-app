7753f429c1f7c39c52d2503a843686732d1b34ee
Mohith Rao <mohith.rao96@gmail.com>
2020-07-02 14:48:01 -0500

    removed login feature

--- frontend/src/components/displays/header.js
+++ frontend/src/components/displays/header.js
@@ -5,10 +5,20 @@ import {headerRoutes} from '../../data/routes'
 import { fire } from '../../data/firebase';
 
 import HeaderLink from './headerLink'
+import dashboard from '../../pages/dashboard';
 
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import logo from './assets/logo.png'
 
 
+const useStyles = makeStyles({
+    root: {
+      color: 'white',
+      height: 48,
+      padding: '0 30px',
+    },
+  });
 
 
 const Header = () => {
@@ -16,11 +26,20 @@ const Header = () => {
 
     const signout = () => {
         fire.auth().signOut();
+        headerRoutes.splice(-1,1)
     }
 
+    if(!!currentUser){
+        headerRoutes.push({
+            path: '/application',
+            label: 'Application',
+            component: dashboard
+          })
+    }
+    const classes = useStyles()
     return(
         <div className='header'>
-            <h1><a href='/'>Arch City Expungement Clinic</a></h1>
+            <h1><a href='/'><img src alt='Expungement'></img></a></h1>
             <div align='center'>
                 {headerRoutes.map((route) => (
                     <HeaderLink 
@@ -30,10 +49,10 @@ const Header = () => {
                     />
                 ))}
 
-                <div className='loginbtn'>
-                    {currentUser ? (<Button onClick={signout}>Sign Out</Button>) : (<Button href='/login'>Login</Button>)}
+                {/* <div className='loginbtn'>
+                    {currentUser ? (<Button onClick={signout} className={classes.root}>Sign Out</Button>) : (<Button href='/login' className={classes.root}>Login</Button>)}
                     
-                </div>
+                </div> */}
             </div>
             
         </div>
