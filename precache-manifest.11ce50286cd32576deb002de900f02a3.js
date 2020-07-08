80e571d4813d7c3d68beb1adee322e19c4317895
Mohith Rao <mohith.rao96@gmail.com>
2020-07-07 20:05:06 -0500

    Updates

---
+++ precache-manifest.11ce50286cd32576deb002de900f02a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3c81c5d5027bd358d840884cd256ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2ba6a80c184c2cd46f7",
+    "url": "/static/css/main.1424c324.chunk.css"
+  },
+  {
+    "revision": "d700a4ebe14c6382e950",
+    "url": "/static/js/2.8a381414.chunk.js"
+  },
+  {
+    "revision": "25b422fbef741e0ef445f544e53d5673",
+    "url": "/static/js/2.8a381414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ba6a80c184c2cd46f7",
+    "url": "/static/js/main.0af820d6.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "69b2987e9e76e8f1c470dd95161f36cf",
+    "url": "/static/media/logo.69b2987e.png"
+  }
+]);
